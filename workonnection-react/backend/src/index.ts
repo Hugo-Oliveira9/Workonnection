7990891628bdb6de041e90fad0bec0d920db8d39
Guilherme <guilheme.silva700@fatec.sp.gov.br>
2026-02-04 10:55:30 -0300

    alteração-de-responsabilidades-e-criação-de-server.ts

--- workonnection-react/backend/src/index.ts
+++ workonnection-react/backend/src/index.ts
@@ -1,22 +1,5 @@
-import express from "express";
-import cors from "cors";
-import authRoutes from "./routes/auth.routes";
+import { app } from "./server";
 
-const app = express();
-const PORT = 3000;
-
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// Rota de teste
-app.get("/", (req, res) => {
-  res.send("Backend Workonnection rodando!");
-});
-
-// Rotas de autenticação
-app.use("/auth", authRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
 });
